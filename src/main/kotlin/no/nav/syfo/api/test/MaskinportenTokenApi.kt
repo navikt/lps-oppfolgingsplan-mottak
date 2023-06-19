026de228d390161f694cdab54bebb2d17686c3bc
Tor-Christian Tangenes <torchrct@gmail.com>
2023-06-19 15:04:19 +0200

    Endpoint for generating access token for maskinporten API

---
+++ src/main/kotlin/no/nav/syfo/api/test/MaskinportenTokenApi.kt
@@ -0,0 +1,95 @@
+package no.nav.syfo.api.test
+
+import com.nimbusds.jose.JOSEObjectType
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.JWSHeader
+import com.nimbusds.jose.crypto.RSASSASigner
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jwt.JWTClaimsSet
+import com.nimbusds.jwt.SignedJWT
+import io.ktor.client.call.*
+import io.ktor.client.request.forms.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.util.*
+import io.ktor.util.*
+import no.nav.syfo.api.util.httpClient
+import no.nav.syfo.environment.Environment
+import org.slf4j.LoggerFactory
+import java.util.*
+
+private const val twoMinutesInSeconds = 120
+private val httpClient = httpClient()
+
+fun Routing.registerMaskinportenTokenApi(
+    env: Environment
+) {
+    route("/api/test/token") {
+        get {
+            val jwtGrant = generateJwtGrant(env)
+            val maskinportenTokenUrl = env.auth.maskinporten.tokenUrl
+            val response: HttpResponse = httpClient.submitForm(
+                url = maskinportenTokenUrl,
+                formParameters = parameters {
+                    append("grant_type", "urn:ietf:params:oauth:grant-type:jwt-bearer")
+                    append("assertion", jwtGrant)
+                }
+            )
+            val maskinportenResponse: MaskinportenResponse = response.body()
+            val maskinPortenAccessToken = maskinportenResponse.access_token
+            val res = maskinPortenAccessToken ?: "ERROR: ${maskinportenResponse.error} || ${maskinportenResponse.error_description}"
+            call.respond(res)
+        }
+    }
+}
+
+private fun generateJwtGrant(env: Environment): String {
+    val authEnv = env.auth.maskinporten
+    val scope = authEnv.scope
+    val audience = authEnv.issuer
+    val clientId = authEnv.clientId
+    val clientJwk = authEnv.clientJwk
+    val rsaKey = RSAKey.parse(clientJwk)
+    val signedJwt = SignedJWT(
+        rsaSignatureFromKey(rsaKey),
+        jwtClaimSet(
+            scope,
+            audience,
+            clientId
+        )
+    )
+    signedJwt.sign(RSASSASigner(rsaKey.toPrivateKey()))
+    return signedJwt.serialize()
+}
+
+private fun rsaSignatureFromKey(key: RSAKey) =
+    JWSHeader.Builder(JWSAlgorithm.RS256)
+        .keyID(key.keyID)
+        .type(JOSEObjectType.JWT)
+        .build()
+
+@OptIn(InternalAPI::class)
+private fun jwtClaimSet(
+    scope: String,
+    audience: String,
+    clientId: String
+): JWTClaimsSet {
+    val now = Date()
+    return JWTClaimsSet.Builder()
+        .audience(audience)
+        .issuer(clientId)
+        .claim("scope", scope)
+        .issueTime(now)
+        .expirationTime(setExpirationTimeTwoMinutesAhead(now))
+        .build()
+
+}
+private fun setExpirationTimeTwoMinutesAhead(issuedAt: Date): Date {
+    val calendar = Calendar.getInstance()
+    calendar.time = issuedAt
+    calendar.add(Calendar.SECOND, twoMinutesInSeconds)
+    return calendar.time
+}
--- src/main/kotlin/no/nav/syfo/environment/Utils.kt
+++ src/main/kotlin/no/nav/syfo/environment/Utils.kt
@@ -2,11 +2,14 @@ package no.nav.syfo.environment
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
+import io.ktor.server.application.*
 import java.io.File
 
 
 const val localPropertiesPath = "./src/main/resources/localEnv.json"
 const val serviceuserMounthPath = "/var/run/secrets"
+const val devCluster = "dev-gcp"
+
 val objectMapper = ObjectMapper().registerKotlinModule()
 
 fun getEnv(): Environment {
@@ -15,12 +18,17 @@ fun getEnv(): Environment {
     }
     return Environment(
         application = ApplicationEnv(
-            port = getEnvVar("APPLICATION_PORT").toInt()
+            port = getEnvVar("APPLICATION_PORT").toInt(),
+            cluster = getEnvVar("NAIS_CLUSTER_NAME")
         ),
         auth = AuthEnv(
             maskinporten = AuthMaskinporten(
                 wellKnownUrl = getEnvVar("MASKINPORTEN_WELL_KNOWN_URL"),
-                scope = getPropertyFromSecretsFile("scope_lps_write")
+                issuer = getEnvVar("MASKINPORTEN_ISSUER"),
+                scope = getEnvVar("MASKINPORTEN_SCOPES"),
+                tokenUrl = getEnvVar("MASKINPORTEN_TOKEN_ENDPOINT"),
+                clientId = getEnvVar("MASKINPORTEN_CLIENT_ID"),
+                clientJwk = getEnvVar("MASKINPORTEN_CLIENT_JWK")
             )
         )
     )
@@ -36,3 +44,7 @@ fun getPropertyFromSecretsFile(name: String) =
 
 fun getLocalEnv() =
     objectMapper.readValue(File(localPropertiesPath), Environment::class.java)
+
+fun Application.isDev(env: Environment, codeToRun: () -> Unit) {
+    if (env.application.cluster == devCluster) codeToRun()
+}
