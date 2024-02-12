e7cc77a6519438d3a6f60cde34e9abbc78eb7e19
Kaja Kvinge Akselsen <75254007+kaja-nav@users.noreply.github.com>
2024-02-12 13:39:26 +0100

    Support Azure Ad On-Behalf-Of tokens (#60)

---
+++ src/test/kotlin/no/nav/syfo/mockdata/MockHttpClient.kt
@@ -0,0 +1,19 @@
+package no.nav.syfo.mockdata
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.mock.MockEngine
+import no.nav.syfo.application.ApplicationEnvironment
+import no.nav.syfo.client.commonConfig
+
+fun mockHttpClient(environment: ApplicationEnvironment) = HttpClient(MockEngine) {
+    commonConfig()
+    engine {
+        addHandler { request ->
+            val requestUrl = request.url.encodedPath
+            when {
+                requestUrl == "/${environment.auth.azuread.accessTokenUrl}" -> azureAdMockResponse()
+                else -> error("Unhandled ${request.url.encodedPath}")
+            }
+        }
+    }
+}
