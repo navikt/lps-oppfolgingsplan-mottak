ad01e08be4d6b023cf21ea68dd6c3be5eae55abd
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2024-02-12 10:03:30 +0100

    New endpoint and DTO for follow-up plans (#55)
    * New endpoint and DTO for follow-up plans

--- src/main/kotlin/no/nav/syfo/oppfolgingsplanmottak/OppfolgingsplanApi.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingsplanmottak/OppfolgingsplanApi.kt
@@ -1,15 +1,14 @@
 package no.nav.syfo.oppfolgingsplanmottak
 
-import io.ktor.server.application.call
-import io.ktor.server.auth.authenticate
-import io.ktor.server.request.receive
-import io.ktor.server.response.respondText
-import io.ktor.server.routing.Routing
-import io.ktor.server.routing.post
-import io.ktor.server.routing.route
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import no.nav.syfo.application.api.auth.JwtIssuerType
 import no.nav.syfo.application.database.DatabaseInterface
 import no.nav.syfo.oppfolgingsplanmottak.database.storeLps
+import no.nav.syfo.oppfolgingsplanmottak.domain.FollowUpPlanDTO
 import no.nav.syfo.oppfolgingsplanmottak.domain.OppfolgingsplanDTO
 
 fun Routing.registerOppfolgingsplanApi(
@@ -22,6 +21,15 @@ fun Routing.registerOppfolgingsplanApi(
                 val virksomhetsnavn = oppfolgingsplanDTO.oppfolgingsplanMeta.virksomhet.virksomhetsnavn
                 database.storeLps(oppfolgingsplanDTO, 1)
                 call.respondText(successText(virksomhetsnavn))
+            }
+        }
+    }
+
+    route("/api/v1/followupplan/write") {
+        authenticate(JwtIssuerType.MASKINPORTEN.name) {
+            post {
+                val followUpPlanDTO = call.receive<FollowUpPlanDTO>()
+                call.respondText("TODO")
             }
         }
     }
