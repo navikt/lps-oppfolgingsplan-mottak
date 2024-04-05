3364499c2df517f81685e3400d3dae8f111da30f
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2024-04-05 11:29:29 +0200

    Improve error handling (#108)

--- src/test/kotlin/no/nav/syfo/oppfolgingsplanmottak/OppfolgingsplanApiTest.kt
+++ src/test/kotlin/no/nav/syfo/oppfolgingsplanmottak/OppfolgingsplanApiTest.kt
@@ -3,6 +3,7 @@ package no.nav.syfo.oppfolgingsplanmottak
 import io.kotest.assertions.ktor.client.shouldHaveStatus
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 import io.ktor.http.*
@@ -12,7 +13,8 @@ import io.mockk.mockk
 import no.nav.syfo.client.isdialogmelding.IsdialogmeldingClient
 import no.nav.syfo.client.oppdfgen.OpPdfGenClient
 import no.nav.syfo.domain.PersonIdent
-import no.nav.syfo.mockdata.createFollowUpPlan
+import no.nav.syfo.mockdata.createDefaultFollowUpPlanMockDTO
+import no.nav.syfo.mockdata.randomFollowUpPlanMockDTO
 import no.nav.syfo.oppfolgingsplanmottak.database.storeLpsPdf
 import no.nav.syfo.oppfolgingsplanmottak.domain.FollowUpPlanResponse
 import no.nav.syfo.util.configureTestApplication
@@ -24,6 +26,10 @@ class OppfolgingsplanApiTest : DescribeSpec({
     val isdialogmeldingConsumer = mockk<IsdialogmeldingClient>(relaxed = true)
     val opPdfGenClient = mockk<OpPdfGenClient>(relaxed = true)
 
+    beforeSpec {
+        coEvery { isdialogmeldingConsumer.sendLpsPlanToGeneralPractitioner(any(), any()) } returns true
+    }
+
     describe("Retrieval of oppfølgingsplaner") {
         val employeeIdentificationNumber = "12345678912"
         val employeeOrgnumber = "123456789"
@@ -33,8 +39,7 @@ class OppfolgingsplanApiTest : DescribeSpec({
             testApplication {
                 val (embeddedDatabase, client) = configureTestApplication()
 
-                val followUpPlanDTO = createFollowUpPlan(employeeIdentificationNumber)
-                coEvery { isdialogmeldingConsumer.sendLpsPlanToGeneralPractitioner(any(), any()) } returns true
+                val followUpPlanDTO = createDefaultFollowUpPlanMockDTO(employeeIdentificationNumber)
                 coEvery { opPdfGenClient.getLpsPdf(any()) } returns pdfByteArray
 
                 val response = client.post("/api/v1/followupplan") {
@@ -54,6 +59,69 @@ class OppfolgingsplanApiTest : DescribeSpec({
                 storedMetaData[0].virksomhetsnummer shouldBe employeeOrgnumber
 
                 response shouldHaveStatus HttpStatusCode.OK
+            }
+        }
+
+        it("Missing lpsName in follow-up plan should return bad request") {
+            testApplication {
+                val (_, client) = configureTestApplication()
+
+                val followUpPlanDTO = randomFollowUpPlanMockDTO.copy(
+                    lpsName = null
+                )
+
+                val response = client.post("/api/v1/followupplan") {
+                    bearerAuth(validMaskinportenToken(consumerOrgnumber = employeeOrgnumber))
+                    contentType(ContentType.Application.Json)
+                    setBody(followUpPlanDTO)
+                }
+                val responseMessage = response.body<String>()
+
+                response shouldHaveStatus HttpStatusCode.BadRequest
+                responseMessage shouldContain "Failed to convert request body"
+            }
+        }
+
+        it("Fails when employee has not contributed, but description is missing") {
+            testApplication {
+                val (_, client) = configureTestApplication()
+
+                val followUpPlanDTO = randomFollowUpPlanMockDTO.copy(
+                    employeeHasContributedToPlan = false,
+                    employeeHasNotContributedToPlanDescription = null
+                )
+
+                val response = client.post("/api/v1/followupplan") {
+                    bearerAuth(validMaskinportenToken(consumerOrgnumber = employeeOrgnumber))
+                    contentType(ContentType.Application.Json)
+                    setBody(followUpPlanDTO)
+                }
+                val responseMessage = response.body<String>()
+
+                response shouldHaveStatus HttpStatusCode.BadRequest
+                responseMessage shouldContain "Failed to convert request body"
+            }
+        }
+
+        it("Fails when needs help from NAV is true, but description is missing") {
+            testApplication {
+                val (_, client) = configureTestApplication()
+
+                val followUpPlanDTO = randomFollowUpPlanMockDTO.copy(
+                    needsHelpFromNav = true,
+                    sendPlanToNav = true,
+                    needsHelpFromNavDescription = null
+                )
+
+                val response = client.post("/api/v1/followupplan") {
+                    bearerAuth(validMaskinportenToken(consumerOrgnumber = employeeOrgnumber))
+                    contentType(ContentType.Application.Json)
+                    setBody(followUpPlanDTO)
+                }
+                val responseMessage = response.body<String>()
+
+                response shouldHaveStatus HttpStatusCode.BadRequest
+                responseMessage shouldContain "Failed to convert request body"
             }
         }
     }
