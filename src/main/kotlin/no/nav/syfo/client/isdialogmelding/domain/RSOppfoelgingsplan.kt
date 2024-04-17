6c0de2d2aa68ce404670bec3602484b6f43cf8c5
Tanja Krasnomovets <victorandreevi4@gmail.com>
2024-04-17 14:14:10 +0200

    wip: code style fix after analyze

--- src/main/kotlin/no/nav/syfo/client/isdialogmelding/domain/RSOppfoelgingsplan.kt
+++ src/main/kotlin/no/nav/syfo/client/isdialogmelding/domain/RSOppfoelgingsplan.kt
@@ -3,4 +3,22 @@ package no.nav.syfo.client.isdialogmelding.domain
 data class RSOppfoelgingsplan(
     val sykmeldtFnr: String,
     val oppfolgingsplanPdf: ByteArray,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RSOppfoelgingsplan
+
+        if (sykmeldtFnr != other.sykmeldtFnr) return false
+        if (!oppfolgingsplanPdf.contentEquals(other.oppfolgingsplanPdf)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = sykmeldtFnr.hashCode()
+        result = 31 * result + oppfolgingsplanPdf.contentHashCode()
+        return result
+    }
+}
--- src/main/kotlin/no/nav/syfo/oppfolgingsplanmottak/domain/FollowUpPlan.kt
+++ src/main/kotlin/no/nav/syfo/oppfolgingsplanmottak/domain/FollowUpPlan.kt
@@ -9,4 +9,29 @@ data class FollowUpPlan(
     fun toFollowUpPlanResponse(): FollowUpPlanResponse {
         return FollowUpPlanResponse(this.uuid, this.isSentToGeneralPractitionerStatus, this.isSentToNavStatus)
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FollowUpPlan
+
+        if (uuid != other.uuid) return false
+        if (isSentToGeneralPractitionerStatus != other.isSentToGeneralPractitionerStatus) return false
+        if (isSentToNavStatus != other.isSentToNavStatus) return false
+        if (pdf != null) {
+            if (other.pdf == null) return false
+            if (!pdf.contentEquals(other.pdf)) return false
+        } else if (other.pdf != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = uuid.hashCode()
+        result = 31 * result + (isSentToGeneralPractitionerStatus?.hashCode() ?: 0)
+        result = 31 * result + (isSentToNavStatus?.hashCode() ?: 0)
+        result = 31 * result + (pdf?.contentHashCode() ?: 0)
+        return result
+    }
 }
