549c85ab5894af2005dfd546f46272ab6818d02c
Luan Tran <8613937+luantr@users.noreply.github.com>
2024-09-20 12:31:09 +0200

    migrate from h2 to testcontainer

--- src/test/kotlin/no/nav/syfo/oppfolgingsplanmottak/database/LpsOppfolgingsplanDaoTest.kt
+++ src/test/kotlin/no/nav/syfo/oppfolgingsplanmottak/database/LpsOppfolgingsplanDaoTest.kt
@@ -1,55 +1,46 @@
 package no.nav.syfo.oppfolgingsplanmottak.database
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import java.sql.SQLException
-import java.util.*
-import no.nav.syfo.db.EmbeddedDatabase
+import no.nav.syfo.db.TestDB
 import no.nav.syfo.mockdata.createDefaultFollowUpPlan
+import java.util.*
 
-class LpsOppfolgingsplanDaoTest : DescribeSpec({
+class LpsOppfolgingsplanDaoTest :
+    DescribeSpec({
+        describe("LpsOppfolgingsplanDaoSpek") {
+            val uuid = UUID.randomUUID()
+            val testDb = TestDB.database
+            beforeTest {
+                TestDB.clearAllData()
+            }
 
-    describe("LpsOppfolgingsplanDaoSpek") {
-        val uuid = UUID.randomUUID()
-        val embeddedDatabase = EmbeddedDatabase()
-        beforeTest {
-            embeddedDatabase.dropData()
-        }
+            it("Should store plan with a given uuid") {
+                testDb.storeFollowUpPlan(
+                    uuid = uuid,
+                    lpsOrgnumber = "123",
+                    organizationNumber = "456",
+                    followUpPlanDTO = createDefaultFollowUpPlan("12121212121"),
+                    sentToNavAt = null,
+                    sentToGeneralPractitionerAt = null,
+                )
+                val fetchedPlan = testDb.findFollowUpPlanResponseById(uuid)
+                fetchedPlan shouldNotBe null
+                fetchedPlan?.uuid shouldBe uuid.toString()
+            }
 
-        it("Should throw exception if no plan with requested uuid is found") {
-            shouldThrow<SQLException> {
-                embeddedDatabase.findFollowUpPlanResponseById(UUID.randomUUID())
+            it("Should return null if plan with a given uuid is not found") {
+                testDb.storeFollowUpPlan(
+                    uuid = uuid,
+                    lpsOrgnumber = "123",
+                    organizationNumber = "456",
+                    followUpPlanDTO = createDefaultFollowUpPlan("12121212121"),
+                    sentToNavAt = null,
+                    sentToGeneralPractitionerAt = null,
+                )
+                val fetchedPlan = testDb.findFollowUpPlanResponseById(UUID.randomUUID())
+                fetchedPlan shouldBe null
             }
         }
-
-        it("Should store plan with a given uuid") {
-            embeddedDatabase.storeFollowUpPlan(
-                uuid = uuid,
-                lpsOrgnumber = "123",
-                organizationNumber = "456",
-                followUpPlanDTO = createDefaultFollowUpPlan("12121212121"),
-                sentToNavAt = null,
-                sentToGeneralPractitionerAt = null,
-            )
-            val fetchedPlan = embeddedDatabase.findSendingStatus(uuid)
-            fetchedPlan shouldNotBe null
-            fetchedPlan?.uuid shouldBe uuid.toString()
-        }
-
-        it("Should return null if plan with a given uuid is not found") {
-            embeddedDatabase.storeFollowUpPlan(
-                uuid = uuid,
-                lpsOrgnumber = "123",
-                organizationNumber = "456",
-                followUpPlanDTO = createDefaultFollowUpPlan("12121212121"),
-                sentToNavAt = null,
-                sentToGeneralPractitionerAt = null,
-            )
-            val fetchedPlan = embeddedDatabase.findSendingStatus(UUID.randomUUID())
-            fetchedPlan shouldBe null
-        }
-    }
-
-})
+    })
