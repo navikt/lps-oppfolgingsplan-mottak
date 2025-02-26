722ec9bcd6154e6cb1b2f38a71c0a4b8bf5d4123
Tanja Krasnomovets <victorandreevi4@gmail.com>
2025-02-26 14:48:15 +0100

    wip

--- src/main/kotlin/no/nav/syfo/client/pdl/PdlClient.kt
+++ src/main/kotlin/no/nav/syfo/client/pdl/PdlClient.kt
@@ -264,7 +264,6 @@ class PdlClient(
                     responseBody.data
                 } else {
                     handlePdlErrors(responseBody.errors, "get person info from PDL")
-                    null // Reached only if error handling doesn't throw
                 }
             }
             HttpStatusCode.NoContent -> throw PdlNotFoundException("No content found in the response body")
@@ -288,7 +287,6 @@ class PdlClient(
                     }
                 } else {
                     handlePdlErrors(responseBody.errors, "get poststed from PDL")
-                    null // Reached only if error handling doesn't throw
                 }
             }
             HttpStatusCode.NoContent -> throw PdlNotFoundException("No content found in the response body")
