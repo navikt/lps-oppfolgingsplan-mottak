c5e5304350fd7be286cae58ee05412a713a7b5b2
Daniel Engebretsen <daniel.engebretsen@nav.no>
2025-08-28 12:05:45 +0200

    avoid wrong navn in AvsenderMottaker (#360)

--- src/main/kotlin/no/nav/syfo/client/dokarkiv/domain/AvsenderMottaker.kt
+++ src/main/kotlin/no/nav/syfo/client/dokarkiv/domain/AvsenderMottaker.kt
@@ -3,5 +3,5 @@ package no.nav.syfo.client.dokarkiv.domain
 data class AvsenderMottaker(
     val id: String,
     val idType: String,
-    val navn: String, // Navnet til avsender/mottaker. Skal være på format Fornavn Mellomnavn Etternavn.
+    val navn: String?, // when idType is ORGNR and navn is null joark will look up the name
 )
