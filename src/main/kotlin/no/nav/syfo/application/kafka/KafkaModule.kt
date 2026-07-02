214facabd9dd260a985befc25871d12f8310f19c
André Wittwer <andre.wittwer@nav.no>
2026-07-02 16:14:41 +0200

    chore: remove topic consumer for aapen-altinn-oppfolgingsplan-mottatt-v2

--- src/main/kotlin/no/nav/syfo/application/kafka/KafkaModule.kt
+++ src/main/kotlin/no/nav/syfo/application/kafka/KafkaModule.kt
@@ -2,8 +2,6 @@ package no.nav.syfo.application.kafka
 
 import io.ktor.server.application.Application
 import kotlinx.coroutines.launch
-import no.nav.syfo.altinnmottak.AltinnLpsService
-import no.nav.syfo.altinnmottak.kafka.AltinnOppfolgingsplanConsumer
 import no.nav.syfo.application.ApplicationState
 import no.nav.syfo.application.Environment
 import no.nav.syfo.sykmelding.SendtSykmeldingAivenConsumer
@@ -13,17 +11,9 @@ import kotlin.coroutines.CoroutineContext
 fun Application.kafkaModule(
     appState: ApplicationState,
     backgroundTasksContext: CoroutineContext,
-    altinnLPSService: AltinnLpsService,
     sykmeldingService: SendtSykmeldingService,
     env: Environment,
 ) {
-    launch(backgroundTasksContext) {
-        launchKafkaListener(
-            appState,
-            AltinnOppfolgingsplanConsumer(env.kafka, altinnLPSService),
-        )
-    }
-
     launch(backgroundTasksContext) {
         launchKafkaListener(
             appState,
